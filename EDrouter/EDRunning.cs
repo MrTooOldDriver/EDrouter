31af4cb44300d273635a25b6b80c75d74ada6505
Enfu Zhang <fanff0324@gmail.com>
2017-08-05 01:31:39 +0800

    Removed the whole EDRunning class and added to EDRunning.cs

---
+++ EDrouter/EDRunning.cs
@@ -0,0 +1,54 @@
+﻿using Reader;
+using System;
+using System.Diagnostics;
+
+namespace EDRunning
+{
+    class EDrunning  //检测游戏是否在运行
+    {
+        public static void EDChecker()
+        {
+            string EDname = "EDLaunch";
+            string EDname2 = "EliteDangerous64";
+            if (Process.GetProcessesByName(EDname).Length >= 1)
+            {
+                Console.WriteLine("ED Launch is running");
+            }
+            else
+            {
+                Console.WriteLine("ED Lanuch is not running");
+                //return;
+            }
+            if (Process.GetProcessesByName(EDname2).Length >= 1)
+            {
+                Console.WriteLine("ED 64 is running");
+                EDlogReader.Reader();
+                LogExtractor.Read();
+                Parser.JSONHandler();
+            }
+            else
+            {
+                Console.WriteLine("ED 64 is not running");
+                EDlogReader.Reader();
+                LogExtractor.Read();
+                Parser.JSONHandler();
+                //return;
+            }
+
+            //  此处由于开发需要把return注释掉了，由于log是需要在游戏启动之后才会生成，所以需要等待游戏启动
+            //  当然这个步骤是在我的思路下写出来的，我还没去研究其他类似软件的源码
+
+            //返回到console，等待调用方法 
+
+            //if (Process.GetProcessesByName = true) ;
+            //Process[] EDrunning = Process.GetProcessesByName(EDname);
+            //double test = int.Parse(EDrunning);
+            //Console.WriteLine(EDrunning);
+            //double test = Process.GetProcessesByName(EDname);
+            //string test = Process[]GetProcessesByName(EDname);
+            //if (Process.GetProcessesByName(EDname) = 0)
+            //{}
+            //以上是已经弃用的代码
+        }
+    }
+}
